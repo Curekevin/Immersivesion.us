3a20b8085946e3b4d47e3dd3f76b4b9c82ec1a38
Kevin Cure <cure1497@gmail.com>
2025-10-31 14:44:10 -0400

    js

---
+++ script.js
@@ -0,0 +1,34 @@
+const boton = document.querySelector('.hero__button');
+
+boton.addEventListener('click', () => {
+    const activo = true;
+    if(activo) {
+        alert('Botón funcionando');
+    } else {
+        alert('Botón no funciona');
+    }
+});
+
+const boton__mid = document.querySelector('.image-text-pair__button');
+
+boton__mid.addEventListener('click', () => {
+    const activo = true;
+    if(activo) {
+        alert('Botón funcionando');
+    } else {
+        alert('Botón no funciona');
+    }
+});
+
+const boton__end = document.querySelector('.final-section__button');
+
+boton__end.addEventListener('click', () => {
+    const activo = true;
+    if(activo) {
+        alert('Botón funcionando');
+    } else {
+        alert('Botón no funciona');
+    }
+});
+
+
